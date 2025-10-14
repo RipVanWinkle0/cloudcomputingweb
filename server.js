d08750085b60a7ffe20c3ae3a42cfb981b2f6a73
Mamchamohammed <Mohammedrayene.mamcha@univ-tlemcen.dz>
2025-10-14 22:31:07 +0100

    Update .gitignore, package.json, and server.js; add PostgreSQL dependency and improve code formatting

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import morgan from "morgan";
-import helmet from "helmet";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -17,14 +15,11 @@ app.use(bodyParser.json());
 app.use(
   cors({
     origin:
-      "https://cloudcomputing3333-bsg7fye5hne0erhu.francecentral-01.azurewebsites.net", // Adjust as needed for your frontend
+      "https://cloudcomputing3333-bsg7fye5hne0erhu.francecentral-01.azurewebsites.net",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-app.use(morgan("combined"));
-app.use(helmet());
-
 // View engine setup
 app.set("view engine", "ejs");
 
@@ -38,6 +33,7 @@ app.get("/", (req, res) => {
   res.render("index", {
     title: "Welcome to My Node.js App",
     message: "Hello from Express + EJS 👋",
+    test: "Test variable",
   });
 });
 
