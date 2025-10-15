b8b6f8fe6f40846c0883ef01908a6df7f7d0767e
Mamchamohammed <Mohammedrayene.mamcha@univ-tlemcen.dz>
2025-10-15 10:34:15 +0100

    Add user listing view and update main route to fetch users from database

--- server.js
+++ server.js
@@ -3,41 +3,31 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
-
-// For __dirname equivalent in ES Modules
+import pool from "./db.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-
-// Middleware
+app.use(cors());
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin:
-      "https://cloudcomputing3333-bsg7fye5hne0erhu.francecentral-01.azurewebsites.net",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-// View engine setup
 app.set("view engine", "ejs");
-
 app.set("views", path.join(__dirname, "views"));
-
-// Static files
 app.use(express.static(path.join(__dirname, "public")));
 
-// Routes
-app.get("/", (req, res) => {
-  res.render("index", {
-    title: "Welcome to My Node.js App",
-    message: "Hello from Express + EJS 👋",
-    test: "Test variable",
-  });
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM users");
+    res.render("users", {
+      title: "User List",
+      users: result.rows,
+      length: result.rows.length,
+    });
+  } catch (err) {
+    console.error("Database error:", err);
+    res.status(500).send("Error fetching users");
+  }
 });
 
-// Server setup
 export const PORT = process.env.PORT || 3000;
 export default app;
 
